db7a3012c147847f1e56c73899a5ea2fb4c90172
soumoud Mizen <soumoud.m@pixelstrade.com>
2020-12-19 17:08:30 +0100

    add new branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'languages',
+    loadChildren: () => import('./pages/languages/languages.module').then( m => m.LanguagesPageModule)
   },
+
 ];
 
 @NgModule({
